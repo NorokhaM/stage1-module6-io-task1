abef147eb4a0eeab7c810f28dcf47830dc663da2
Maksym Norokha <maxnorohich@gmail.com>
2024-02-26 21:01:11 +0100

    123

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,35 @@
 package com.epam.mjc.io;
 
+import java.io.BufferedReader;
 import java.io.File;
-
+import java.io.IOException;
 
 public class FileReader {
-
-    public Profile getDataFromFile(File file) {
-        return new Profile();
+    public Profile getDataFromFile(File file)  {
+        BufferedReader reader = null;
+        int index=0;
+        String[] data = new String[4];
+        try{
+            reader = new BufferedReader(new java.io.FileReader(file));
+            String readLine;
+            while ((readLine = reader.readLine()) != null) {
+                data[index++] = readLine;
+            }
+            String name = data[0].split(":")[1].trim();
+            Integer age= Integer.parseInt(data[1].split(":")[1].trim());
+            String email = data[2].split(":")[1].trim();
+            Long phone = Long.parseLong(data[3].split(":")[1].trim());
+            return new Profile(name, age, email, phone);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        finally {
+            try {
+                reader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
     }
 }
