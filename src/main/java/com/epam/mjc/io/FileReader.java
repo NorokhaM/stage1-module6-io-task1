da8524af6630e1902ef8576a9196085138bf3a0f
Maksym Norokha <maxnorohich@gmail.com>
2024-02-26 21:07:02 +0100

    123

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -6,11 +6,12 @@ import java.io.IOException;
 
 public class FileReader {
     public Profile getDataFromFile(File file)  {
-        BufferedReader reader = null;
+        if (file == null) {
+            throw new IllegalArgumentException("File is null");
+        }
         int index=0;
         String[] data = new String[4];
-        try{
-            reader = new BufferedReader(new java.io.FileReader(file));
+        try(BufferedReader reader = new BufferedReader(new java.io.FileReader(file))){
             String readLine;
             while ((readLine = reader.readLine()) != null) {
                 data[index++] = readLine;
@@ -19,16 +20,10 @@ public class FileReader {
             Integer age= Integer.parseInt(data[1].split(":")[1].trim());
             String email = data[2].split(":")[1].trim();
             Long phone = Long.parseLong(data[3].split(":")[1].trim());
+            reader.close();
             return new Profile(name, age, email, phone);
         } catch (Exception e) {
             e.printStackTrace();
-        }
-        finally {
-            try {
-                reader.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
         }
         return null;
     }
